3871eecf165e20984c4cc75757d5370016398cd7
LumenWang <lumenwang@163.com>
2019-11-20 00:15:04 +0800

    添加了两行输出语句

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -3,5 +3,7 @@ package com.main;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("用Eclipse创建的Java项目");
+		System.out.println("用Eclipse创建的Java项目");
+		System.out.println("用Eclipse创建的Java项目");
 	}
 }
