54b795188b7980a91fcaa7b235035f1c2a3eeae3
LumenWang <lumenwang@163.com>
2019-12-07 20:38:24 +0800

    添加了两个抽象类 Employee 和 Student

---
+++ src/com/tools/Employee.java
@@ -0,0 +1,19 @@
+package com.tools;
+
+public class Employee {
+	public String name;
+	public int age;
+	public String address;
+	public double salary;
+
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(String name, int age, String address, double salary) {
+		this.name = name;
+		this.age = age;
+		this.address = address;
+		this.salary = salary;
+	}
+}
---
+++ src/com/tools/Student.java
@@ -0,0 +1,19 @@
+package com.tools;
+
+public class Student {
+	public int id;
+	public double score;
+	public String name;
+	public String grade;
+
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int id, double score, String name, String grade) {
+		this.id = id;
+		this.score = score;
+		this.name = name;
+		this.grade = grade;
+	}
+}
